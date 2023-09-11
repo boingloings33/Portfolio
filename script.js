cc313b1b87fea63c5cc516e208a95bb50193c7a7
boingloings33 <brenden.baio@codeandtrust.com>
2023-09-11 02:01:05 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+jQuery(function () {
+  $(".head").on("click", function () {
+    let isActive = $(this).hasClass("active");
+    $(".head").removeClass("active");
+    $(".head").parent().find(".content").slideUp(280);
+    $(".head").parent().find(".arrow").removeClass("arrow-animate");
+    if (isActive == false) {
+      $(this).addClass("active");
+      $(this).parent().find(".arrow").addClass("arrow-animate");
+      $(this).parent().find(".content").slideDown(280);
+    }
+  });
+  $("img").on("mouseenter", function () {
+    $("h1").toggle();
+  });
+  $("img").on("mouseleave", function () {
+    $("h1").toggle(20);
+  });
+});
