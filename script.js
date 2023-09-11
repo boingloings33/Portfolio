7559eeb1a2807472d248df684ad2acebc7ce7581
boingloings33 <brenden.baio@codeandtrust.com>
2023-09-11 04:03:58 -0400

    scroll into view changes, preview pic adjustments

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 jQuery(function () {
   $(".head").on("click", function () {
     let isActive = $(this).hasClass("active");
+    this.scrollIntoView({ behavior: "smooth", inline: "nearest" });
     $(".head").removeClass("active");
     $(".head").parent().find(".content").slideUp(280);
     $(".head").parent().find(".arrow").removeClass("arrow-animate");
-    if (isActive == false) {
+    if (isActive === false) {
       $(this).addClass("active");
       $(this).parent().find(".arrow").addClass("arrow-animate");
       $(this).parent().find(".content").slideDown(280);
